ef7f71b649329a4481316b45c77ae49fcaa5f521
JuanJo Rey <juanreyfotos@gmail.com>
2021-05-02 16:07:31 +0200

    Este es mi primer commit desde intelliJ

---
+++ src/PruebaVideo.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class PruebaVideo {
+    private JTextField textField1;
+    private JButton miVentanaButton;
+    private JTextField textField2;
+    private JButton pruebaButton;
+}
---
+++ src/ventana.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ventana {
+    private JButton button1;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton miVentanaButton;
+    private JTextArea textArea1;
+
+    public ventana() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+}
